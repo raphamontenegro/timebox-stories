3f1801a3a69034f4ca323e4119b59a1c70d7a9df
Germán Rodríguez <germanrodriguez@MacBook-Pro-de-German.local>
2018-05-10 15:08:47 +0200

    css forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-
+// Pages
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 import { notFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+// Services 
 import { AuthService } from './services/auth.service';
 
+// Guards
+import { InitAuthGuardService } from './guards/init-auth-guard.service';
+import { RequireAnonGuardService } from './guards/require-anon-guard.service';
+import { RequireUserGuardService } from './guards/require-user-guard.service';
+
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'signup', component: SignupPageComponent },
+  { path: '', component: HomePageComponent, canActivate:  [InitAuthGuardService] },
+  { path: 'login', component: LoginPageComponent, canActivate: [InitAuthGuardService] },
+  { path: 'signup', component: SignupPageComponent, canActivate: [InitAuthGuardService] },
   { path: '**', component: notFoundPageComponent }
 ];
 
@@ -34,7 +41,12 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    InitAuthGuardService,
+    RequireAnonGuardService,
+    RequireUserGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
