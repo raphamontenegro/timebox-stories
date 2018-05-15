e12b1aeede785b5450687876e3b2b905e40da097
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-05-15 19:54:12 +0200

    pocket user defined, stories half protected

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -43,7 +43,7 @@ export class LoginPageComponent implements OnInit {
     }
   }
 
-  callPocket() { 
+  callPocket() {
     this.error = '';
     this.processing = true;
     this.authService.pocketLogin()
--- src/app/pages/stories-length-page/stories-length-page.component.ts
+++ src/app/pages/stories-length-page/stories-length-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StoriesService } from '../../services/stories.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-stories-length-page',
@@ -11,13 +11,16 @@ export class StoriesLengthPageComponent implements OnInit {
   stories: any;
   storiesLength: number;
 
-  constructor(private storiesService: StoriesService, private activatedRoute: ActivatedRoute) {}
+  constructor(private storiesService: StoriesService, private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.storiesLength = params.time;
       this.storiesService.getAllByLength(this.storiesLength)
       .then(data => {
+        if (!data.length) {
+          this.router.navigate(['/']);
+        }
         this.stories = data;
       });
     });
